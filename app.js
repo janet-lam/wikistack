9b425ea4b3aab1f2fa8047eb379878cedfea5a46
Janet Lam <janetlam3933@gmail.com>
2020-11-19 16:47:16 -0500

    connect database and define tables

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 const express = require("express");
 const morgan = require("morgan");
+const layout = require("./views/layout");
+const { db, User, Page } = require("./models");
 const app = express();
 
+const PORT = 3000;
+
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
+
+const init = async () => {
+  await db.sync();
+  //   await db.sync({ force: true });
+
+  //   same as db.sync()
+  //   await Page.sync();
+  //   await User.sync();
+
+  //  Why is an async function inside another async function? Is app.listen an async function?
+  //  How does this work?
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`);
+  });
+};
+init();
+
+// try this out after we're done:
+// db.sync();
+
 app.use(morgan("dev"));
-app.use(express.static("/public"));
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send(layout(""));
 });
 
-const PORT = 1337;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Listening on port ${PORT}`);
+// });

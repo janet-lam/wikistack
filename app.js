c79d1e18fd377309541e03ee8d44b7f5ad63dff8
Janet Lam <janetlam3933@gmail.com>
2020-11-19 13:19:12 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.static("/public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+const PORT = 1337;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
